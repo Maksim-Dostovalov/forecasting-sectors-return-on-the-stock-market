1b05a26e61875ddd35a4de890ab84367e789ec1e
Dostovalov Maksim <maksim.dostovalov@outlook.com>
2024-02-22 21:58:51 +0300

    Загрузил котировки индексов

--- load-data.py
+++ load-data.py
@@ -31,3 +31,33 @@ data_stocks = pd.DataFrame({key: pd.DataFrame(value).set_index('TRADEDATE')['CLO
 
 data_stocks.to_csv('./data/data_stocks_moex.csv')
 stocks_info.to_csv('./data/stocks_info.csv')
+
+indexes = [
+    'MOEXRE', # Строительство
+    'MOEXMM', # Металлургия
+    'MOEXIT', # Информационные технологии
+    'MOEXFN', # Финансы 
+    'MOEXEU', # Электроэнергетика
+    'MOEXCN', # Потребительский сектор
+    'MOEXCH', # Нефтехимическая промышленность
+    'MOEXOG', # Нефть и газ
+    'MOEXTL', # Телекоммуникации
+    'MOEXTN'  # Транспорт
+    ]
+
+with requests.Session() as session:
+    dictionary = {}
+    for secid in tqdm(indexes):
+        data = apimoex.get_market_history(
+            session, 
+            secid,  
+            market='index', 
+            columns=('TRADEDATE', 'CLOSE')
+        )
+        dictionary[secid] = data
+
+
+
+data_indexes = pd.DataFrame({key: pd.DataFrame(value).set_index('TRADEDATE')['CLOSE'] for key, value in dictionary.items()})
+
+data_indexes.to_csv('./data/data_indexes_moex.csv')
