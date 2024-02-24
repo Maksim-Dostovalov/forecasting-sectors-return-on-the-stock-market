40ed871564d2a8355ad6edd45a3978d937d19116
Dostovalov Maksim <maksim.dostovalov@outlook.com>
2024-02-25 00:28:02 +0300

    Пример STL декомпозиции

---
+++ ts_decomposition.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.seasonal import STL
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('./data/data_indexes_moex.csv', index_col=0)
+data.index = pd.to_datetime(data.index)
+
+ts = data['MOEXMM'].interpolate(method='time')
+
+# Преобразование ежедневных данных в месячные, используя значение за месяц
+ts_monthly = ts.resample('M').sum()
+
+decompose = STL(ts_monthly)
+result = decompose.fit()
+
+result.plot()
+plt.show()
